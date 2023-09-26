3dce49343e748f98c59f626a4d5dd73462059615
Rishabh Porwal <137980586+devops433@users.noreply.github.com>
2023-09-26 05:33:06 +0000

    conversion_of_datatypes

---
+++ 01_Basics/conversion_operation.js
@@ -0,0 +1,13 @@
+// let score = "Rishabh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
